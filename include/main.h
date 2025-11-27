ecd9d83a5dd1641e4961038347ec3d70611d9be6
MrNNP <reachnp64@gmail.com>
2025-11-27 05:16:41 -0500

    it works

--- include/main.h
+++ include/main.h
@@ -35,11 +35,13 @@
 #define PROS_USE_LITERALS
 
 #include "api.h"
+#include "serial_read.h"
+#include <ctime>
 
 /**
  * You should add more #includes here
  */
-//#include "okapi/api.hpp"
+// #include "okapi/api.hpp"
 
 /**
  * If you find doing pros::Motor() to be tedious and you'd prefer just to do
@@ -59,13 +61,14 @@
  * button press in opcontrol() for testing purposes).
  */
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
-void autonomous(void);
-void initialize(void);
-void disabled(void);
-void competition_initialize(void);
-void opcontrol(void);
+    void autonomous(void);
+    void initialize(void);
+    void disabled(void);
+    void competition_initialize(void);
+    void opcontrol(void);
 #ifdef __cplusplus
 }
 #endif
@@ -74,7 +77,7 @@ void opcontrol(void);
 /**
  * You can add C++-only headers here
  */
-//#include <iostream>
+// #include <iostream>
 #endif
 
-#endif  // _PROS_MAIN_H_
+#endif // _PROS_MAIN_H_
--- include/serial_read.h
+++ include/serial_read.h
@@ -1,5 +1,11 @@
-#include "api.h"
+#include "pros/apix.h"
+#include "cobs.h"
+#include "CRC.h"
+#include "msg.pb.h"
+
 #include <vector>
 using namespace std;
+
+extern DataMessage serial_data;
 
 void readit();
